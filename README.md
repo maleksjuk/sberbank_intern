dbf8624e30d833d8474caed672e215ad53b83dfb
maleksjuk <maleksjuk@mail.ru>
2020-06-10 04:32:22 +0800

    add task 2

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ __Задача min:__
 
 ### Как запустить
 
+1. Основная задача
+
 Открыть файл Districts_of_Moscow.ipynb в jupyter-notebook или ввести в терминале:
 
     python3 Districts_of_Moscow.py 
@@ -24,3 +26,19 @@ __Задача min:__
     pip3 install pandas
     
 Чтобы просмотреть отчёт (без скачивания), нужно открыть файл Districts_of_Moscow.ipynb или Distinct_of_Moscow.html 
+
+
+2. Задача определения района по адресу
+
+Ввести в терминале:
+
+    python3 get_district_moscow.py
+
+Для успешного запуска и выполнения проекта нужно, чтобы были установлены библиотеки __json__, __geojson__, __shapely__ и __requests__:
+
+    pip3 install json
+	pip3 install geojson
+	pip3 install Shapely
+	pip3 install requests
+
+
---
+++ get_district_moscow.py
@@ -0,0 +1,49 @@
+import json
+import geojson
+import shapely.geometry as shg
+import requests
+
+print("Загрузка данных: ", end='')
+with open('mo.geojson') as f:
+    data_gj_mo = geojson.load(f)
+print("OK")
+
+def get_district(point):
+    
+    point = shg.Point(point)
+    result = ""
+    
+    for feature in data_gj_mo['features']:
+        polygon = shg.shape(feature['geometry'])
+        if polygon.contains(point):
+            result = feature['properties']['TYPE_MO'] + ' ' + feature['properties']['NAME']
+            break
+    return result
+
+def get_coord(address):
+    api_url = "https://geocode-maps.yandex.ru/1.x"
+    api_key = 'e774a1c0-7bd2-488a-a88d-facab89278a9'
+    params = {'geocode': 'Москва ' + address, 'apikey': api_key, 'format': 'json'}
+    response = requests.get(api_url, params)
+    res = response.json()['response']['GeoObjectCollection'] \
+        ['featureMember'][0]['GeoObject']['Point']['pos'].split()
+    return [float(res[0]), float(res[1])]
+
+print("""
+    ОПРЕДЕЛЕНИЕ МУНИЦИПАЛЬНОГО ОКРУГА МОСКВЫ ПО АДРЕСУ
+
+    Адрес необходимо ввести без указания города Москва
+    (иначе можно получить неверные результаты).
+    В программе используются данные районов с сайта 
+    https://gis-lab.info/qa/moscow-atd.html,
+    а также Геокодер API Яндекс.Карт.
+""")
+
+finish = True
+while finish:
+    address = input("\nВведите адрес (например, Охотный ряд 1): ")
+    if len(address) > 0:
+        print(get_district(get_coord(address)))
+    check = input("\nПродолжить? (да/[нет]): ")
+    if check != 'да':
+        finish = False
